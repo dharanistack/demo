a2966b6b9d71c31fa7f170bcc5e337dcbd9604b7
stackgen-automation <automation@stackgen.com>
2025-10-31 05:38:03 +0000

    Changes pushed by stackgen for appstack: d05d5922-3472-4d90-8bc3-a217d16e7398

---
+++ terraform/env/production.tfvars
@@ -0,0 +1,20 @@
+# Module: ./modules/aws_subnet (stackgen_134452a9-1bc6-42b4-b1d0-70753f4c4a09)
+availability_zones   = ["us-east-1a", "us-east-1b"]
+enable_dns_hostnames = true
+enable_dns_support   = true
+environment          = "production"
+private_subnet_cidrs = ["10.0.10.0/24", "10.0.11.0/24"]
+public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
+region               = "us-east-1"
+vpc_cidr             = "10.0.0.0/16"
+
+# Module: ./modules/aws_internet_gateway (stackgen_3ac79643-b65b-4ecd-8082-abf5d66f223e)
+
+# Module: ./modules/aws_subnet (stackgen_92e09b63-c33a-4c20-a838-cf4df09dc851)
+
+# Module: ./modules/aws_subnet (stackgen_a9282d55-2ac2-4ada-8107-960b87fa5b53)
+
+# Module: ./modules/aws_vpc (stackgen_b61e2aa2-00d1-4a32-beb2-fa35cbec13a2)
+
+# Module: ./modules/aws_subnet (stackgen_decf33ab-453d-4790-aefb-43674ab9efdd)
+
---
+++ terraform/outputs.tf
@@ -0,0 +1,75 @@
+output "aws_internet_gateway_stackgen_3ac79643-b65b-4ecd-8082-abf5d66f223e_arn" {
+  value     = module.stackgen_3ac79643-b65b-4ecd-8082-abf5d66f223e.arn
+  sensitive = false
+}
+
+output "aws_internet_gateway_stackgen_3ac79643-b65b-4ecd-8082-abf5d66f223e_id" {
+  value     = module.stackgen_3ac79643-b65b-4ecd-8082-abf5d66f223e.id
+  sensitive = false
+}
+
+output "aws_subnet_stackgen_134452a9-1bc6-42b4-b1d0-70753f4c4a09_arn" {
+  value     = module.stackgen_134452a9-1bc6-42b4-b1d0-70753f4c4a09.arn
+  sensitive = false
+}
+
+output "aws_subnet_stackgen_134452a9-1bc6-42b4-b1d0-70753f4c4a09_id" {
+  value     = module.stackgen_134452a9-1bc6-42b4-b1d0-70753f4c4a09.id
+  sensitive = false
+}
+
+output "aws_subnet_stackgen_decf33ab-453d-4790-aefb-43674ab9efdd_arn" {
+  value     = module.stackgen_decf33ab-453d-4790-aefb-43674ab9efdd.arn
+  sensitive = false
+}
+
+output "aws_subnet_stackgen_decf33ab-453d-4790-aefb-43674ab9efdd_id" {
+  value     = module.stackgen_decf33ab-453d-4790-aefb-43674ab9efdd.id
+  sensitive = false
+}
+
+output "aws_subnet_stackgen_92e09b63-c33a-4c20-a838-cf4df09dc851_arn" {
+  value     = module.stackgen_92e09b63-c33a-4c20-a838-cf4df09dc851.arn
+  sensitive = false
+}
+
+output "aws_subnet_stackgen_92e09b63-c33a-4c20-a838-cf4df09dc851_id" {
+  value     = module.stackgen_92e09b63-c33a-4c20-a838-cf4df09dc851.id
+  sensitive = false
+}
+
+output "aws_subnet_stackgen_a9282d55-2ac2-4ada-8107-960b87fa5b53_arn" {
+  value     = module.stackgen_a9282d55-2ac2-4ada-8107-960b87fa5b53.arn
+  sensitive = false
+}
+
+output "aws_subnet_stackgen_a9282d55-2ac2-4ada-8107-960b87fa5b53_id" {
+  value     = module.stackgen_a9282d55-2ac2-4ada-8107-960b87fa5b53.id
+  sensitive = false
+}
+
+output "aws_vpc_stackgen_b61e2aa2-00d1-4a32-beb2-fa35cbec13a2_arn" {
+  value     = module.stackgen_b61e2aa2-00d1-4a32-beb2-fa35cbec13a2.arn
+  sensitive = false
+}
+
+output "aws_vpc_stackgen_b61e2aa2-00d1-4a32-beb2-fa35cbec13a2_default_network_acl_id" {
+  value     = module.stackgen_b61e2aa2-00d1-4a32-beb2-fa35cbec13a2.default_network_acl_id
+  sensitive = false
+}
+
+output "aws_vpc_stackgen_b61e2aa2-00d1-4a32-beb2-fa35cbec13a2_default_route_table_id" {
+  value     = module.stackgen_b61e2aa2-00d1-4a32-beb2-fa35cbec13a2.default_route_table_id
+  sensitive = false
+}
+
+output "aws_vpc_stackgen_b61e2aa2-00d1-4a32-beb2-fa35cbec13a2_id" {
+  value     = module.stackgen_b61e2aa2-00d1-4a32-beb2-fa35cbec13a2.id
+  sensitive = false
+}
+
+output "aws_vpc_stackgen_b61e2aa2-00d1-4a32-beb2-fa35cbec13a2_vpc_id" {
+  value     = module.stackgen_b61e2aa2-00d1-4a32-beb2-fa35cbec13a2.vpc_id
+  sensitive = false
+}
+
